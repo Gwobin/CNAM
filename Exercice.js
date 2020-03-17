fda0f52cfac8b0b99af28a58233c3314bffd68a6
Gwobin <patrice.racaud.auditeur@lecnam.net>
2020-03-17 19:22:03 +0100

    Exercice 1 pour CNAM

---
+++ Exercice.js
@@ -0,0 +1,17 @@
+// Define the class with constructor
+
+var Author = /** @class */ (function () {
+    function Author(Name) {
+        this.Name = Name;
+    }
+    return Author
+}());
+
+// create the instance of the class
+
+var author = new Author("Patrice Racaud");
+
+//print the information in the console
+
+console.log("\nHello Cnam");
+console.log (author .Name);
